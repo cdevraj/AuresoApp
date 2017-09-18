d1020ea819b25064d5a3e57a21f8d71eda0d8495
Dinesh <geekdinesh25@gmail.com>
2017-09-19 03:30:16 +0530

    time zone added

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,13 +1,40 @@
 class Track < ActiveRecord::Base
 
-scope :by_name, ->(name) { where(name: name) }
+	scope :by_name, ->(name) { where(name: name) }
 
-  def get_metadata(max_speed_of_car)
-  	return 0 unless max_speed_of_car
-    max_speed_on_track = max_speed_of_car - percent_of(max_speed_of_car)
+	def get_metadata(max_speed_of_car, time_zone: 'CET')
+		return 0 unless max_speed_of_car
+		max_speed_on_track = max_speed_of_car - (percent_of(max_speed_of_car) + slow_down_for_zone(max_speed_of_car, time_zone))
+	end
+
+	def slow_down_for_zone(max_speed_of_car, time_zone)
+  	# get current time in the zone
+  	# if falls in the difference 
+  	# retun slow down factor speed
+  	time = set_time_zone do
+  		p Time.zone.now
+  	end
+  	percentage = case time
+  	when time.hour > 9  && time.hour <= 18
+  		0
+  	when time.hour > 18 && (time.hour <= 21 && time.minute <=30)
+  		8
+  	when (time.hour > 21 && time.minute > 30) && time.hour <= 6
+  		15
+  	when time.hour > 6 && time.hour <= 9
+  		8
+  	else
+  		0  		
+  	end
+  	percent_of(max_speed_of_car, factor: percentage)
   end
 
-   def percent_of(max_speed_of_car)
-    (self.slow_factor.to_f / max_speed_of_car.to_f ) * 100.0
+  def set_time_zone(&block)
+  	time_zone = self&.time_zone || 'CET'
+  	Time.use_zone(time_zone, &block)
+  end
+
+   def percent_of(max_speed_of_car, factor: self.slow_factor.to_f)
+    ( factor/max_speed_of_car.to_f )*100.0
   end
 end
