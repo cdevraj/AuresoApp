3e7a94dd80dbd491b1c89a77661631ef423034af
Dinesh <geekdinesh25@gmail.com>
2017-09-19 02:07:55 +0530

    rspec testcases and refactors

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,15 +1,13 @@
 class Api::V1::CarsController < ApplicationController
-  respond_to :json
+	respond_to :json
 
-  def index
-    puts params[:track]
-      car = Car.by_slug(params[:slug]).first
-      track = Track.find_by_name(params[:track]) if params[:track]
-    if car
-    	render json: {data: {car: CarSerializer.new(car, {track: track})}}
-    else
-      render json: {error: "car #{params[:slug]} could not be found"}
-    end
-  end
-
+	def index
+		car = Car.by_slug(params[:slug]).first
+		track = Track.by_name(params[:track]).first if params[:track]
+		if car
+			render json: {data: {car: CarSerializer.new(car, {track: track})}}
+		else
+			render json: {error: "car #{params[:slug]} could not be found"}
+		end
+	end
 end
