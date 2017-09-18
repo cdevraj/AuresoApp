c3c8fff450548980a62100d6728482da03805ea2
Dinesh <geekdinesh25@gmail.com>
2017-09-19 02:11:44 +0530

    Reindented with 2 spaces

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::CarsController < ApplicationController
   respond_to :json
 
   def show
-    puts params[:track]
-      car = Car.by_slug(params[:slug]).first
-      track = Track.find_by_name(params[:track]) if params[:track]
+    car = Car.by_slug(params[:slug]).first
+    track = Track.by_name(params[:track]).first if params[:track]
     if car
-    	render json: {data: {car: CarSerializer.new(car, {track: track})}}
+      render json: {data: {car: CarSerializer.new(car, {track: track})}}
     else
       render json: {error: "car #{params[:slug]} could not be found"}
     end
