1bcea76f16b7b838ca59c6de3153797369c61964
Dinesh <geekdinesh25@gmail.com>
2017-09-19 15:40:00 +0530

    controller v2 added

--- app/controllers/api/v2/cars_controller.rb
+++ app/controllers/api/v2/cars_controller.rb
@@ -1,10 +1,8 @@
-class Api::V1::CarsController < ApplicationController
-  # respond_to :json
-
+class Api::V2::CarsController < ApplicationController
   def show
     car = Car.friendly.find(params[:id]) rescue nil
     if car
-      render json: {data: {car: CarSerializer.new(car, {track_name: params[:track], with_time_zone: true})}}
+      render json: {data: {car: CarSerializer.new(car, {track_name: params[:track]})}}
     else
       render json: {error: "car #{params[:id]} could not be found"}
     end
