738d79ed3c24ba7e22456ccf49941160293e6735
Dinesh <geekdinesh25@gmail.com>
2017-09-19 15:41:12 +0530

    track model removed unwnated method

--- app/models/track.rb
+++ app/models/track.rb
@@ -8,35 +8,12 @@ class Track < ActiveRecord::Base
     max_speed_on_track - percent_of(max_speed_of_car, factor: get_slow_down_percentage.to_f) 
   end
 
-
-  def slow_down_for_zone(max_speed_of_car, time_zone)
-    # get current time in the zone
-    # if falls in the difference 
-    # retun slow down factor speed
-    zone_time = DateTime.now.in_time_zone(time_zone)
-    begining_of_day =  zone_time.beginning_of_day
-    percentage = 0
-
-    if zone_time.between?(begining_of_day+9.hours, begining_of_day+18.hours)
-      percentage = 0
-    elsif zone_time.between?(begining_of_day+(18.hours+1.second), begining_of_day+(21.hours+30.minutes))
-      percentage = 8
-    elsif zone_time.between?(begining_of_day+(21.hours+30.minutes+1.second), begining_of_day+6.hours)
-      percentage = 15
-    elsif zone_time.between?(begining_of_day+6.hours, begining_of_day+(9.hours+30.minutes))
-      percentage = 8
-    else
-      puts "else"
-      percentage = 0
-    end        
-    percent_of(max_speed_of_car, factor: percentage.to_f)  
-  end
-
   def percent_of(max_speed_of_car, factor: self.slow_factor.to_f)
     ( factor / max_speed_of_car.to_f )*100.0
   end
 
   def get_slow_down_percentage
+    zone_time = DateTime.now.in_time_zone(time_zone)
     begining_of_day = DateTime.now.in_time_zone(time_zone).beginning_of_day
     return 0 if zone_time.between?(begining_of_day + 9.hours, begining_of_day + 18.hours)
     return 15 if zone_time.between?(begining_of_day + (21.hours + 30.minutes + 1.second), begining_of_day + 1.day + 6.hours)
