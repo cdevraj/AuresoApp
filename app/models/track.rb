94e4c04adff1c082d4ffde017f8e6778c51a79e7
Dinesh <geekdinesh25@gmail.com>
2017-09-19 14:01:49 +0530

    track

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,31 +1,45 @@
 class Track < ActiveRecord::Base
   scope :by_name, ->(name) { where(name: name) }
   
-  def get_metadata(max_speed_of_car)
+  def get_metadata(max_speed_of_car, time_zone: 'CET')
     return 0 unless max_speed_of_car
-    max_speed_on_track = max_speed_of_car - percent_of(max_speed_of_car)
+    max_speed_on_track = max_speed_of_car - (percent_of(max_speed_of_car) + slow_down_for_zone(max_speed_of_car, time_zone))
   end
 
+
   def slow_down_for_zone(max_speed_of_car, time_zone)
-    # get current time in the zone
-    # if falls in the difference 
-    # retun slow down factor speed
-    time = set_time_zone do
-      p Time.zone.now
-    end
-    percentage = case time
-    when time.hour > 9  && time.hour <= 18
+      # get current time in the zone
+      # if falls in the difference 
+      # retun slow down factor speed
+    # zone_time = set_time_zone do
+    #   p Time.zone.now
+    # end
+    # puts zone_time.time
+    zone_time = DateTime.now.in_time_zone(time_zone)
+    puts zone_time
+    begining_of_day =  zone_time.beginning_of_day#zone_time.time.beginning_of_day
+    # byebug
+
+    percentage = case zone_time
+    when zone_time.between?(begining_of_day+9.hours, begining_of_day+18.hours)
       0
-    when time.hour > 18 && (time.hour <= 21 && time.minute <=30)
+    when zone_time.between?(begining_of_day+(18.hours+1.second), begining_of_day+(21.hours+30.minutes))
+      # 6pm – 9.30pm | 8%
+      puts "6pm – 9.30pm"
       8
-    when (time.hour > 21 && time.minute > 30) && time.hour <= 6
+    when zone_time.between?(begining_of_day+(21.hours+30.minutes+1.second), begining_of_day+6.hours)
+      # 9.30pm – 6am | 15%
+      puts "9.30pm – 6am"
       15
-    when time.hour > 6 && time.hour <= 9
+    when zone_time.between?(begining_of_day+6.hours, begining_of_day+(9.hours+30.minutes))
+      # 6am – 9am | 8%  
+      puts "6am – 9am"
       8
     else
+      puts "else"
+      raise "error"
       0     
     end
-    percent_of(max_speed_of_car, factor: percentage)
   end
 
   def set_time_zone(&block)
