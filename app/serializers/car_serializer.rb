5fe80532541ec84bf59d8a8111680ac72591af20
Dinesh <geekdinesh25@gmail.com>
2017-09-19 11:13:35 +0530

    reindented with 2 spaces

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,13 +1,13 @@
 class CarSerializer < ActiveModel::Serializer
-	attributes :id, :slug, :max_speed, :max_speed_on_track  
+  attributes :id, :slug, :max_speed, :max_speed_on_track  
 
-	def max_speed_on_track
-		return 'no track selected' if @instance_options[:track].blank?
-		"#{@instance_options[:track].get_metadata(object.max_speed).to_i} " + "Km/hr"
-	end
+  def max_speed_on_track
+    return 'no track selected' if @instance_options[:track].blank?
+    "#{@instance_options[:track].get_metadata(object.max_speed).to_i} " + "Km/hr"
+  end
 
-	def max_speed
-		return "0 Km/hr" unless object.max_speed
-		"#{object.max_speed} Km/hr"
-	end
+  def max_speed
+    return "0 Km/hr" unless object.max_speed
+    "#{object.max_speed} Km/hr"
+  end
 end
